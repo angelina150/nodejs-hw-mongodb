a3d99c4fd1ef7bd44701a215d6294e730b3de2d2
Angelina Chigiryova <chigiryova31@gmail.com>
2025-01-18 22:00:08 +0200

    fix2

--- src/utils/parsePaginationParams.js
+++ src/utils/parsePaginationParams.js
@@ -13,11 +13,3 @@ export const parsePaginationParams = ({ page, perPage }) => {
     perPage: parsedPerPage,
   };
 };
-export const parsePaginationParams2 = ({ page, perPage }) => {
-  const parsedPage = parseNumber(page, 1);
-  const parsedPerPage = parseNumber(perPage, 10);
-  return {
-    page: parsedPage,
-    perPage: parsedPerPage,
-  };
-};
